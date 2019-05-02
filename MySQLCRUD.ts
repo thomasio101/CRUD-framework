f787966da8c08c557e09901356130ab25842cfee
Thomas Egbert Duursma <thomasduursma@outlook.com>
2019-05-02 19:14:58 +0200

    Moved commands to below the MySQLCRUD class
    Moved the namespace containing the MySQLCRUD commands below the code containing the MySQLCRUD class.
    
    Also seperated the "export default" statement from the MySQLCRUD class's definition.

--- MySQLCRUD.ts
+++ MySQLCRUD.ts
@@ -1,13 +1,9 @@
 import CRUDImplementation from './CRUDImplementation';
 import * as mysql from 'mysql2/promise';
 
-namespace MySQLCRUD {
-    export type CreateCommand<T, identifierColumn extends keyof T> = Pick<T, Exclude<keyof T, identifierColumn>>
-    export type UpdateCommand<T, identifierColumn extends keyof T> = Pick<T, Extract<keyof T, identifierColumn>> & Partial<Pick<T, Exclude<keyof T, identifierColumn>>>
-    export type DeleteCommand<T, identifierColumn extends keyof T> = Pick<T, Extract<keyof T, identifierColumn>>
-}
+type AtLeastOne<T, U = {[K in keyof T]: Pick<T, K> }> = Partial<T> & U[keyof U]
 
-export default class MySQLCRUD<T, identifierColumn extends keyof T> implements CRUDImplementation<T, number, MySQLCRUD.CreateCommand<T, identifierColumn>, MySQLCRUD.UpdateCommand<T, identifierColumn>, MySQLCRUD.DeleteCommand<T, identifierColumn>> {
+class MySQLCRUD<T, identifierColumn extends keyof T> implements CRUDImplementation<T, number, MySQLCRUD.CreateCommand<T, identifierColumn>, MySQLCRUD.UpdateCommand<T, identifierColumn>, MySQLCRUD.DeleteCommand<T, identifierColumn>> {
     constructor(private connectionPool: mysql.Pool, private table: string, private identifierColumn: string) { };
 
     async get(identifer: number): Promise<T> {
@@ -84,3 +80,10 @@ export default class MySQLCRUD<T, identifierColumn extends keyof T> implements C
         connection.release()
     }
 }
+namespace MySQLCRUD {
+    export type CreateCommand<T, identifierColumn extends keyof T> = Pick<T, Exclude<keyof T, identifierColumn>>
+    export type UpdateCommand<T, identifierColumn extends keyof T> = Pick<T, Extract<keyof T, identifierColumn>> & Partial<Pick<T, Exclude<keyof T, identifierColumn>>>
+    export type DeleteCommand<T, identifierColumn extends keyof T> = Pick<T, Extract<keyof T, identifierColumn>>
+}
+
+export default MySQLCRUD
